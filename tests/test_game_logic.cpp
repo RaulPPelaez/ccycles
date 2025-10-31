910cc50a947a91e65f4a812bf2c259e46458c330
RaulPPealez <raul.perez@uam.es>
2025-10-31 09:55:37 +0100

    chore: remove old comments

--- tests/test_game_logic.cpp
+++ tests/test_game_logic.cpp
@@ -1,56 +1,11 @@
-//GTest tests for game logic
-#include"server/game_logic.h"
-#include"gtest/gtest.h"
-#include<fstream>
+// GTest tests for game logic
+#include "server/game_logic.h"
+#include "gtest/gtest.h"
+#include <fstream>
 using cycles::Id;
 using namespace cycles_server;
-// Game Logic
-// class Game {
-//   const Configuration conf;
-//   uint max_tail_length = 55;
-//   Id idCounter = 1;
-//   int frame = 0;
-//   bool gameStarted = false;
-//   std::map<Id, Player> players;
-//   std::vector<sf::Uint8> grid;
-//   std::mt19937 rng;
-//   std::mutex gameMutex;
 
-// public:
-//   Game(Configuration conf)
-//       : conf(conf), grid(conf.gridWidth * conf.gridHeight, 0),
-//         rng(std::random_device()()) {}
-
-//   Id addPlayer(const std::string &name);
-
-//   void removePlayer(Id id);
-
-//   void movePlayers(std::map<Id, Direction> directions);
-
-//   const auto &getGrid() { return grid; }
-
-//   auto getPlayers() {
-//     std::scoped_lock lock(gameMutex);
-//     return players;
-//   }
-
-//   void setFrame(int frame) { this->frame = frame; }
-
-//   int getFrame() { return frame; }
-
-//   bool isGameOver() { return gameStarted && players.size() = 1; }
-
-// private:
-
-//   Id &getCell(int x, int y) { return grid[y * conf.gridWidth + x]; }
-
-//   bool legalMove(sf::Vector2i newPos);
-
-//   std::set<Id> checkCollisions(std::map<Id, sf::Vector2i> newPositions);
-
-// };
-
-std::string writeConfig(){
+std::string writeConfig() {
   std::string conf_yaml = R"(
 gameHeight: 1000
 gameWidth: 1000
@@ -61,11 +16,12 @@ maxClients: 60
 )";
   auto temp_file = std::tmpnam(nullptr);
   std::ofstream out(temp_file);
-  out<<conf_yaml;
+  out << conf_yaml;
   return temp_file;
 }
 
-bool test_grid(std::vector<sf::Uint8> grid, std::map<Id, Player> players, Configuration conf) {
+bool test_grid(std::vector<sf::Uint8> grid, std::map<Id, Player> players,
+               Configuration conf) {
   int GRID_HEIGHT = conf.gridHeight;
   int GRID_WIDTH = conf.gridWidth;
   std::vector<sf::Uint8> true_grid(GRID_HEIGHT * GRID_WIDTH, 0);
@@ -123,11 +79,13 @@ TEST(GameLogicTest, MovePlayers) {
   auto players_before = game.getPlayers();
   game.movePlayers(directions);
   auto players = game.getPlayers();
-  EXPECT_EQ(players[id].position, players_before[id].position + sf::Vector2i(0, -1));
-  EXPECT_EQ(players[id2].position, players_before[id2].position + sf::Vector2i(0, 1));
+  EXPECT_EQ(players[id].position,
+            players_before[id].position + sf::Vector2i(0, -1));
+  EXPECT_EQ(players[id2].position,
+            players_before[id2].position + sf::Vector2i(0, 1));
 }
 
-TEST(GameLogicTest, GameOver){
+TEST(GameLogicTest, GameOver) {
   // Write some yaml conf to a temp file
   std::string conf_file = writeConfig();
   Configuration conf(conf_file);
@@ -140,7 +98,7 @@ TEST(GameLogicTest, GameOver){
   EXPECT_TRUE(game.isGameOver());
 }
 
-TEST(GameLogicTest, Grid){
+TEST(GameLogicTest, Grid) {
   // Write some yaml conf to a temp file
   std::string conf_file = writeConfig();
   Configuration conf(conf_file);
